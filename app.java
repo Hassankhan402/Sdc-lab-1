a0a648d45cbd7f3319fed6db2c40aa457f6f2ace
Hassankhan402 <barryharry484@gmail.com>
2024-10-21 15:24:49 +0500

    Update app.java

--- app.java
+++ app.java
@@ -2,23 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JoinLists {
-
     public static void main(String[] args) {
-
-        List<String> list1 = new ArrayList<String>();
-        list1.add("a");
-
-        List<String> list2 = new ArrayList<String>();
-        list2.add("b");
-
-        List<String> joined = new ArrayList<String>();
-
-        joined.addAll(list1);
+        // Initialize list1 with elements
+        List<String> list1 = new ArrayList<>(List.of("a", "c", "d"));
+        
+        // Initialize list2 with elements
+        List<String> list2 = new ArrayList<>(List.of("b", "e"));
+        
+        // Create a new list to hold the joined results
+        List<String> joined = new ArrayList<>(list1);
         joined.addAll(list2);
 
+        // Print the contents of each list
         System.out.println("list1: " + list1);
         System.out.println("list2: " + list2);
         System.out.println("joined: " + joined);
-
     }
 }
